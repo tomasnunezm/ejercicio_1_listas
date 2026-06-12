7b5ef24a8b2cfc937be1485b6da44e531da7889e
tomasnunezm <tom.nunezm@duocuc.cl>
2026-06-12 09:45:26 -0400

    commit

---
+++ ejercicio1.py
@@ -0,0 +1,4 @@
+""" El complejo educacional “Chile 2030”, desea realizar una aplicación computacional que le permita registrar en sus establecimientos los alumnos a sus cursos.
+Por tal razón le ha solicitado que cree un programa que le permita a coordinación estudiantil registrar los alumnos que pertenezcan a un curso en particular.
+Como prototipo, usted desarrolla un algoritmo que permite almacenar un número variable de alumnos a un curso, pero con un máximo de 30 por curso.
+Construya el código que responda a los siguientes resultados """
